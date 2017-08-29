6c5e4356e19b1764f995f89ca6c6952f16061202
kattakapilkumar <kapilkatta123@gmail.com>
2017-08-29 22:35:29 +0530

    hackerrank commit

---
+++ src/daywisecoding30/Loops.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Loops {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int res=0;
+        for(int i=1;i<=10;i++)
+        {
+        	res = n * i;
+        	System.out.println(n+" "+"x"+" "+i+" "+"="+" "+res);
+        }
+
+	}
+
+}
