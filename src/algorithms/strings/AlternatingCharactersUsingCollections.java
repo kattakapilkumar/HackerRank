7917486ed2825776cee95f37f4b3f361032598f4
kattakapilkumar <kapilkatta123@gmail.com>
2017-09-10 08:09:23 +0530

    new commit

--- src/algorithms/strings/AlternatingCharactersUsingCollections.java
+++ src/algorithms/strings/AlternatingCharactersUsingCollections.java
@@ -1,10 +1,35 @@
 package algorithms.strings;
 
+
+import java.util.*;
 public class AlternatingCharactersUsingCollections {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		boolean katta =false;
+		Scanner kapil=new Scanner(System.in);		
+		int n=kapil.nextInt();
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			count=0;
+			String s=kapil.next();
+			Set cs=new HashSet();
+			for(int x=0;x<s.length();x++)
+			{
+				katta=cs.add(s.charAt(x));
+				if(!(katta))
+				{
+					count++;
+				}
+				
+			}
+			System.out.println(count);
+			
+			
+		}
+		
 	}
 
 }
